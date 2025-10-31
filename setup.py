3fa4be9b38f2fad14877adfd2d3a7db5768aca79
Sifat Hasan <sihabhossan633@gmail.com>
2025-10-31 15:29:17 +0600

    Updated

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
     name="langchat",
     version="0.0.1",
-    author="Sifat Hasan <sihabhossan633@gmail.com>, NeuroBrain <contact@neurobrain.co>",
+    author="Sifat Hasan <sihabhossan633@gmail.com>, NeuroBrain",
     author_email="contact@neurobrain.co",
     description="A conversational AI library with vector search capabilities",
     long_description=long_description,
