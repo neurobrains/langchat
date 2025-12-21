bcda45938ecb285c9d8c78a89496d45c5fc2a5b7
Pro-Sifat-Hasan <sihabhossan633@gmail.com>
2025-12-21 21:48:34 +0600

    License: add MIT License header to all source files
    Copyright (c) 2025 NeuroBrain Co Ltd.
    
    Signed-off-by: Pro-Sifat-Hasan <sihabhossan633@gmail.com>

--- src/langchat/adapters/vector_db/__init__.py
+++ src/langchat/adapters/vector_db/__init__.py
@@ -17,7 +17,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-    
+
 from langchat.adapters.vector_db.pinecone_adapter import PineconeVectorAdapter
 
 __all__ = ["PineconeVectorAdapter"]
--- src/langchat/exceptions.py
+++ src/langchat/exceptions.py
@@ -18,6 +18,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+
 class UnsupportedFileTypeError(Exception):
     """
     Exception raised when a file type is not supported.
