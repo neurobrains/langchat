396cb8a3ce96ee7f415702f6d34d8024e379c7d7
Sifat Hasan <sihabhossan633@gmail.com>
2026-01-01 07:49:13 +0600

    Change copyright and license in lint.py
    Updated copyright information and license type in lint.py.

--- scripts/lint.py
+++ scripts/lint.py
@@ -1,22 +1,9 @@
-# Copyright 2025 Emcie Co Ltd.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Copyright (c) 2025 NeuroBrain Co Ltd.
+# Licensed under the MIT License.
 
 import sys
 from functools import partial
 
-# Note: This script requires a utils module with Package, die, and for_each_package
-# If these utilities are not available, this script may not work as expected
 try:
     from utils import Package, die, for_each_package  # type: ignore[import-untyped]
 except ImportError:
